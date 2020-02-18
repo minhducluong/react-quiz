a6b461134c5692b497f6c5e1e7962082e1f7bbd0
minhducluong <duclm.94@gmail.com>
2020-02-18 16:41:00 +0700

    Updates

---
+++ precache-manifest.a013b882b007d9be0a9023e83f50c31c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d127074511df5b0157402d01b36ea22",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "f6c52e4b66d58c5fef17",
+    "url": "/react-quiz/static/css/main.30079cf8.chunk.css"
+  },
+  {
+    "revision": "3c3d36325bb8f45de63a",
+    "url": "/react-quiz/static/js/2.4f771414.chunk.js"
+  },
+  {
+    "revision": "f6c52e4b66d58c5fef17",
+    "url": "/react-quiz/static/js/main.037682c0.chunk.js"
+  },
+  {
+    "revision": "0737b3bac2a6204b8bad",
+    "url": "/react-quiz/static/js/runtime~main.813f9954.js"
+  },
+  {
+    "revision": "758abfbed6c792c5a9c06f9bbf1e41bf",
+    "url": "/react-quiz/static/media/background.758abfbe.jpg"
+  }
+]);
